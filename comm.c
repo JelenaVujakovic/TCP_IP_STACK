f68083481512a170339d535f3c41437f9df09195
JelenaVujakovic <44649153+JelenaVujakovic@users.noreply.github.com>
2021-06-12 08:12:50 +0200

    Update comm.c

--- comm.c
+++ comm.c
@@ -1,37 +1,4 @@
-/*
- * =====================================================================================
- *
- *       Filename:  comm.c
- *
- *    Description:  This file contains the routines to implement the communication between nodes
- *
- *        Version:  1.0
- *        Created:  Thursday 19 September 2019 10:31:35  IST
- *       Revision:  1.0
- *       Compiler:  gcc
- *
- *         Author:  Er. Abhishek Sagar, Networking Developer (AS), sachinites@gmail.com
- *        Company:  Brocade Communications(Jul 2012- Mar 2016), Current : Juniper Networks(Apr 2017 - Present)
- *        
- *        This file is part of the NetworkGraph distribution (https://github.com/sachinites).
- *        Copyright (c) 2017 Abhishek Sagar.
- *        This program is free software: you can redistribute it and/or modify
- *        it under the terms of the GNU General Public License as published by  
- *        the Free Software Foundation, version 3.
- *
- *        This program is distributed in the hope that it will be useful, but 
- *        WITHOUT ANY WARRANTY; without even the implied warranty of 
- *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- *        General Public License for more details.
- *
- *        You should have received a copy of the GNU General Public License 
- *        along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * =====================================================================================
- */
-
-#include "comm.h"
-#include "graph.h"
+#include "comm.h"#include "graph.h"
 #include <sys/socket.h>
 #include <pthread.h>
 #include <netinet/in.h>
