125ab94a44ef5dbc5e63c06de41e33220b17a30d
JelenaVujakovic <44649153+JelenaVujakovic@users.noreply.github.com>
2021-07-04 09:38:04 +0200

    Generic graph construction

--- testapp.c
+++ testapp.c
@@ -4,11 +4,6 @@
 
 extern graph_t *build_first_topo();
 extern graph_t *build_simple_l2_switch_topo();
-extern graph_t *build_square_topo();
-extern graph_t *build_linear_topo();
-extern graph_t *build_dualswitch_topo();
-extern graph_t *linear_3_node_topo();
-extern graph_t *L2_loop_topo();
 extern void nw_init_cli();
 
 graph_t *topo = NULL;
@@ -18,7 +13,6 @@ main(int argc, char **argv){
 
     nw_init_cli();
 	show_help_handler(0, 0, MODE_UNKNOWN);
-    topo = build_square_topo();
-    start_shell(); 
+    topo = build_square_topo(); 
     return 0;
 }
