e0621a3930255fa185643fb4a3e39fff7ade7b03
JelenaVujakovic <44649153+JelenaVujakovic@users.noreply.github.com>
2021-07-04 09:39:55 +0200

    Complete data structure which represents data graph

--- graph.h
+++ graph.h
@@ -47,80 +47,9 @@ typedef struct graph_{
     glthread_t node_list; 
 } graph_t;
 
-node_t *
-create_graph_node(graph_t *graph, char *node_name);
-
-graph_t *
-create_new_graph(char *topology_name);
-
-void
-insert_link_between_two_nodes(node_t *node1, 
-                             node_t *node2,
-                             char *from_if_name, 
-                             char *to_if_name, 
-                             unsigned int cost);
-
-/*Helper functions*/
-static inline node_t *
-get_nbr_node(interface_t *interface){
-
-    assert(interface->att_node);
-    assert(interface->link);
-    
-    link_t *link = interface->link;
-    if(&link->intf1 == interface)
-        return link->intf2.att_node;
-    else
-        return link->intf1.att_node;
-}
-
-static inline int
-get_node_intf_available_slot(node_t *node){
-
-    int i ;
-    for( i = 0 ; i < MAX_INTF_PER_NODE; i++){
-        if(node->intf[i])
-            continue;
-        return i;
-    }
-    return -1;
-}
-
-static inline interface_t *
-get_node_if_by_name(node_t *node, char *if_name){
-
-    int i ;
-    interface_t *intf;
-
-    for( i = 0 ; i < MAX_INTF_PER_NODE; i++){
-        intf = node->intf[i];
-        if(!intf) return NULL;
-        if(strncmp(intf->if_name, if_name, IF_NAME_SIZE) == 0){
-            return intf;
-        }
-    }
-    return NULL;
-}
-
-static inline node_t *
-get_node_by_node_name(graph_t *topo, char *node_name){
-
-    node_t *node;
-    glthread_t *curr;    
-
-    ITERATE_GLTHREAD_BEGIN(&topo->node_list, curr){
-
-        node = graph_glue_to_node(curr);
-        if(strncmp(node->node_name, node_name, strlen(node_name)) == 0)
-            return node;
-    } ITERATE_GLTHREAD_END(&topo->node_list, curr);
-    return NULL;
-}
-
 /*Display Routines*/
 void dump_graph(graph_t *graph);
 void dump_node(node_t *node);
 void dump_interface(interface_t *interface);
-
 
 #endif /* __NW_GRAPH_ */
